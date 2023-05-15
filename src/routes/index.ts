2beea17648ebcbc376e67e490257f7a25bbb2f2a
Gustavo Morais <gustavomorais47.gm@gmail.com>
2023-05-15 11:15:41 -0300

    Atualização com o CRUD completo do User

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,12 +5,13 @@ import reserveRouter from "./reserve";
 import roomRouter from "./room";
 import userRouter from "./user";
 import Auth from "../controller/Auth";
+import authMiddleware from "../middleware/auth";
 
 const router = Router();
 
 router.post('/login', (req, res) => Auth(req, res));
 
-router.use('/user', userRouter);
+router.use('/user', authMiddleware ,userRouter);
 router.use('/called', calledRouter);
 router.use('/reserve', reserveRouter);
 router.use('/room', roomRouter);
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,28 +1,29 @@
-import { Request , Response, Router } from "express";
-import authMiddleware from "../middleware/auth";
+import { Router } from "express";
 import getAll from "../controller/User/getAll";
 import getOneUser from "../controller/User/getOneUser";
 import createUser from "../controller/User/createUser";
+import updateOneUser from "../controller/User/updateOneUser";
+import updatePasswordUser from "../controller/User/updatePasswordUser";
+import deleteOneUser from "../controller/User/deleteOneUser";
 
 const userRouter = Router();
 
 //retorna todos os usuários do sistema
-userRouter.get('/', authMiddleware, getAll);
+userRouter.get('/', getAll);
 
 //retorna um usuário em específico
-userRouter.get('/:cpf', authMiddleware, getOneUser);
+userRouter.get('/:cpf', getOneUser);
 
 // cadastra um novo usuário no sistema
-userRouter.post('/', authMiddleware, createUser);
+userRouter.post('/', createUser);
 
 // atualiza um usuário em específico
-userRouter.put('/:id', authMiddleware, (req: Request, res: Response) => {
+userRouter.put('/:id', updateOneUser);
 
-});
+// atualiza a senha de um usuário em específico
+userRouter.put('/password/:id', updatePasswordUser);
 
 // deleta um usuário em específico, irreversível
-userRouter.delete('/:id', authMiddleware, (req: Request, res: Response) => {
-
-});
+userRouter.delete('/:id', deleteOneUser);
 
 export default userRouter;
